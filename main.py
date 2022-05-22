9195f93c9bc155dfa17c905ea34dbbf0570f8dda
michaelkitas <mixaliskitas@gmail.com>
2022-05-22 09:40:56 +0300

    How to Create General & Admin Commands

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord # discord.py module
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message): # When a message is sent
+        prefix = ".mike." #Replace with your prefix
+        if message.content.startswith(prefix): # If the message starts with the prefix
+            command = message.content[len(prefix):] # Get the command
+            isAdmin = [role.name == "Admin" for role in message.author.roles][0] # Check if the user is an admin
+            # Check for commands
+            if command == "hello" and isAdmin:
+                await message.channel.send("Hello Admin!") # Send a message
+
+            if command == "help":
+                await message.channel.send("```\n"
+                                           "Commands:\n"
+                                           "help - This is the help command\n"
+                                           "stats - It returns the server stats\n"
+                                           "```")
+            else:
+                # If the command is not found
+                await message.channel.send("This command doesn't exist") 
+                       
+
+
+
+client = MyClient()
+client.run('OTcwMjk3NDIwMDk1Mjk1NTM4.Ym555A.NNUbexXoBVxqFbkuIPD50znQPXg') # Replace with your token
