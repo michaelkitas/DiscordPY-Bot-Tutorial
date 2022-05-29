3b5d055e106e69fa22966e0c6f665d47afbb09da
michaelkitas <mixaliskitas@gmail.com>
2022-05-29 06:25:02 +0300

    Add Restricted Channels

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-import discord # discord.py module
+import discord  # discord.py module
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message): # When a message is sent
-        prefix = ".mike." #Replace with your prefix
-        if message.content.startswith(prefix): # If the message starts with the prefix
-            command = message.content[len(prefix):] # Get the command
-            isAdmin = [role.name == "Admin" for role in message.author.roles][0] # Check if the user is an admin
-            # Check for commands
-            if command == "hello" and isAdmin:
-                await message.channel.send("Hello Admin!") # Send a message
+    async def on_message(self, message):  # When a message is sent
+        channel = message.channel.name
+        restricted_channels = ["bot-commands"] # List of restricted channels
 
-            if command == "help":
-                await message.channel.send("```\n"
-                                           "Commands:\n"
-                                           "help - This is the help command\n"
-                                           "stats - It returns the server stats\n"
-                                           "```")
+        prefix = ".mike."  # Replace with your prefix
+        # If the message starts with the prefix
+        if message.content.startswith(prefix):
+            if channel in restricted_channels: # If the message was sent in a restricted channel
+                command = message.content[len(prefix):]  # Get the command
+                # Check if the user is an admin
+                isAdmin = [role.name ==
+                           "Admin" for role in message.author.roles][0]
+                # Check for commands
+                if command == "hello" and isAdmin:
+                    # Send a message
+                    await message.channel.send("Hello Admin!")
+
+                if command == "help":
+                    await message.channel.send("```\n"
+                                               "Commands:\n"
+                                               "help - This is the help §erver stats\n"
+                                               "```")
+
+                else:
+                    # If the command is not found
+                    await message.channel.send("This command doesn't exist")
             else:
-                # If the command is not found
-                await message.channel.send("This command doesn't exist") 
-                       
-
+                await message.delete()
+                
+                # await message.channel.send("You can't use commands in this channel")
 
 
 client = MyClient()
-client.run('######') # Replace with your token
+# Replace with your token
+client.run('YOUR_TOKEN_GOES_HERE')
